87c7b3570e80bf5b2402903f595e34a595565855
mdjinnah akondo <mdjinnahakondo@gmail.com>
2026-02-27 15:22:08 +0600

    general links added

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -3,6 +3,10 @@ import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
 import { PiCalendarDotDuotone } from "react-icons/pi";
 import { TbBrandGoogleAnalytics } from "react-icons/tb";
 import { HiOutlineUsers } from "react-icons/hi";
+import { CiSettings } from "react-icons/ci";
+import { MdLogout } from "react-icons/md";
+import { IoHelpBuoyOutline } from "react-icons/io5";
+
 import { NavLink } from "react-router";
 export default function Sidebar() {
   const navLinks = [
@@ -12,8 +16,14 @@ export default function Sidebar() {
     { label: "Analytics", icon: TbBrandGoogleAnalytics, to: "/" },
     { label: "Team", icon: HiOutlineUsers, to: "/" },
   ];
+
+  const generalLinks = [
+    { label: "Settings", icon: CiSettings, to: "/settings" },
+    { label: "Help", icon: IoHelpBuoyOutline, to: "/" },
+    { label: "Logout", icon: MdLogout, to: "/" },
+  ];
   return (
-    <div className="bg-base-200 h-[calc(100vh-40px)] max-w-48 rounded-3xl p-8">
+    <div className="bg-base-200 min-h-[calc(100vh-40px)] max-w-48 rounded-3xl p-8">
       {/* logo  */}
       <h2 className="text-lg font-semibold  mb-16">Dashboard</h2>
       {/* manu links */}
@@ -25,12 +35,25 @@ export default function Sidebar() {
           <NavLink
             key={i}
             to={link.to}
-            className={`flex items-center gap-4 py-3 relative transition`}
+            className={`flex items-center gap-4 py-3 relative transition text-base-content/70`}
           >
-            <span>
-              <link.icon size={24} />
-            </span>
-            {link.label}
+            <link.icon size={24} />
+            <span className="hidden md:block">{link.label}</span>
+          </NavLink>
+        ))}
+      </ul>
+      <h4 className="uppercase text-sm text-base-content/60 font-medium mt-12 ">
+        General
+      </h4>
+      <ul>
+        {generalLinks.map((link, i) => (
+          <NavLink
+            key={i}
+            to={link.to}
+            className={`flex items-center gap-4 py-3 relative transition text-base-content/70`}
+          >
+            <link.icon size={24} />
+            <span className="hidden md:block">{link.label}</span>
           </NavLink>
         ))}
       </ul>
